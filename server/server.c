eb3321240521fd3cecdfc68d2dfa1116551ef025
christoph <c.moser400@gmail.com>
2024-02-28 19:49:00 +0100

    Minor cleanup in include statements

--- server/server.c
+++ server/server.c
@@ -1,6 +1,3 @@
-//needed for accept4()
-#define _GNU_SOURCE
-
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -8,16 +5,6 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include "server.h"
-
-#define MAX_PENDING 1
-#define BUFFER_SIZE 512
-
-#define MAX_CLIENTS 1
-
-#define OUTPUT_CHANNEL stdout
-#define INPUT_CHANNEL stdin
-#define ERROR_CHANNEL stderr
-
 
 int main() {
     fprintf(OUTPUT_CHANNEL, "Starting up the server... \n");
@@ -29,22 +16,20 @@ int main() {
 
     size_t num_connected = 0;
     //start listening
-    while(1) {
+    while (1) {
 
         sleep(5);
-        fprintf(OUTPUT_CHANNEL, "Waiting for client message... \n");
 
         // accept client connections on the socket if more clients are still possible
         if (num_connected < MAX_CLIENTS) {
             struct sockaddr_in client_address;
             int addr_len = sizeof(client_address);
-            int new_client_fd = accept(socket_fd, (struct sockaddr *)&client_address,
-                                   (socklen_t *)&addr_len);
+            int new_client_fd = accept(socket_fd, (struct sockaddr *) &client_address,
+                                       (socklen_t *) &addr_len);
 
             if (new_client_fd == -1) {
-                //fprintf(ERROR_CHANNEL, "Connection to client failed \n");
-            }
-            else {
+                //fprintf(ERROR_CHANNEL, "No client connected\n");
+            } else {
                 fprintf(OUTPUT_CHANNEL, "Successfully accepted client \n");
                 client_connections[num_connected] = new_client_fd;
                 num_connected++;
@@ -53,26 +38,24 @@ int main() {
 
         //read message for any of the connected clients
         for (size_t client_id = 0; client_id < num_connected; client_id++) {
+            fprintf(OUTPUT_CHANNEL, "Polling connected clients for messages... \n");
             size_t charbuf_size = (BUFFER_SIZE + 1) * sizeof(char);
             char *message_buffer = calloc(charbuf_size, 1);
             ssize_t num_read_bytes = read(client_connections[client_id], message_buffer, charbuf_size);
             if (num_read_bytes > 0) {
-                fprintf(OUTPUT_CHANNEL, "Received message from client %zu: %s \n", client_id,  message_buffer);
-            }
-            else {
+                fprintf(OUTPUT_CHANNEL, "Received message from client %zu: %s \n", client_id, message_buffer);
+            } else {
                 fprintf(OUTPUT_CHANNEL, "No message received from client %zu \n", client_id);
             }
+            free(message_buffer);
         }
+
+
 
         //close(client_fd);
     }
 
-
-
-    connection_t connection;
-    run_server(connection);
-    //open_all_connections(connections);
-    //clean_connections();
+    
 
     return -1;
 }
@@ -96,7 +79,7 @@ int setup_server_socket() {
     struct sockaddr_un server_address;
     memset(&server_address, 0, sizeof(server_address));
     server_address.sun_family = AF_UNIX;
-    strncpy(server_address.sun_path, SOCKET_PATH, sizeof (server_address.sun_path) - 1);
+    strncpy(server_address.sun_path, SOCKET_PATH, sizeof(server_address.sun_path) - 1);
 
     // bind socket to address
     int bind_ret = bind(socket_fd, (struct sockaddr *) &server_address, sizeof(server_address) - 1);
--- server/server.h
+++ server/server.h
@@ -1,17 +1,21 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#define MAX_PENDING 1
+#define BUFFER_SIZE 512
+
+#define MAX_CLIENTS 1
+
+#define OUTPUT_CHANNEL stdout
+#define INPUT_CHANNEL stdin
+#define ERROR_CHANNEL stderr
+
 #include "../util/connection.h"
-
-void read_client_message(connection_t connection);
-
-void listen_to_messages();
 
 void run_server(connection_t connection);
 
-void open_all_connections(connection_t **connections);
-
 int setup_server_socket();
+
 
 
 
