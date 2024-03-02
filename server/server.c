b125ccaaca1b97b130796c3d74d1747507d1397d
christoph <c.moser400@gmail.com>
2024-03-02 21:15:54 +0100

    Used select to multiplex read on server-end

--- server/server.c
+++ server/server.c
@@ -21,23 +21,12 @@ int main() {
     while (1) {
 
         sleep(5);
-
         // accept client connections on the socket if more clients are still possible
         if (num_connected < MAX_CLIENTS) {
             struct sockaddr_in client_address;
             int addr_len = sizeof(client_address);
             int new_client_fd = accept(socket_fd, (struct sockaddr *) &client_address,
                                        (socklen_t *) &addr_len);
-
-            //Replace accept by select to monitor multiple file descriptors:
-            fd_set read_sockets;
-            // set read timeout for after which reading on a single socket stops
-            struct timeval timeout;
-            timeout.tv_sec = 1;
-            timeout.tv_usec = 0;
-            int read_fds = select(FD_SETSIZE, &read_sockets, (fd_set *) NULL,
-                                  (fd_set *) NULL, &timeout);
-
             if (new_client_fd == -1) {
                 // case where no new client is connecting
                 fprintf(ERROR_CHANNEL, "No client connected\n");
@@ -50,9 +39,22 @@ int main() {
             fprintf(OUTPUT_CHANNEL, "Maximal number of clients connected: %zu/%d \n", num_connected, MAX_CLIENTS);
         }
 
-        //read message for any of the connected clients
-        for (size_t client_id = 0; client_id < num_connected; client_id++) {
-            fprintf(OUTPUT_CHANNEL, "Polling connected clients for messages... \n");
+        fd_set readset;
+        int num_readset = get_available_sockets(&readset, client_connections, num_connected);
+        if (num_readset == 0) {
+            // no messages waiting to be received
+            continue;
+        }
+        else {
+            fprintf(OUTPUT_CHANNEL, "Receiving client messages \n");
+        }
+        for (size_t client_id = 0; client_id < num_connected; ++client_id) {
+
+            // check whether fd is in readset
+            if (!FD_ISSET(client_connections[client_id], &readset)) {
+                continue;
+            }
+
             size_t charbuf_size = (BUFFER_SIZE + 1) * sizeof(char);
             char *message_buffer = calloc(charbuf_size, 1);
             ssize_t num_read_bytes = read(client_connections[client_id], message_buffer, charbuf_size);
@@ -63,9 +65,10 @@ int main() {
             }
             free(message_buffer);
         }
-        //close(client_fd);
+
     }
-    return -1;
+
+    return 0;
 }
 
 void run_server(connection_t connection) {
@@ -106,12 +109,34 @@ int setup_server_socket() {
 }
 
 void cleanup_server(int *client_fd, int num_connected, int socket_fd) {
-    //TODO: Cleanup
+    //TODO: Cleanup and add with on_exit
     //close all file descriptors to clients
     for (int i = 0; i < num_connected; ++i) {
         close(client_fd[i]);
     }
-
     close(socket_fd);
     unlink(SOCKET_PATH);
+}
+
+int get_available_sockets(fd_set *sockets, int *monitored_descriptors, size_t num_monitored) {
+    //Replace accept by select to monitor multiple file descriptors:
+    //fd_set sockets;
+    // set read timeout for after which reading on a single socket stops
+
+    //initially reset socket set
+    FD_ZERO(sockets);
+
+    //register all connected fds into the readset
+    for (size_t i = 0; i < num_monitored; ++i) {
+        FD_SET(*(monitored_descriptors + i), sockets);
+    }
+
+    struct timeval timeout;
+    timeout.tv_sec = SELECT_TIMEOUT_SEC;
+    timeout.tv_usec = SELECT_TIMEOUT_MICROSEC;
+
+    // get all fds that contain data to be read
+    int num_read_fds = select(FD_SETSIZE, sockets, (fd_set *) NULL,
+                              (fd_set *) NULL, &timeout);
+    return num_read_fds;
 }
--- server/server.h
+++ server/server.h
@@ -3,8 +3,10 @@
 
 #define MAX_PENDING 1
 #define BUFFER_SIZE 512
-
 #define MAX_CLIENTS 2
+
+#define SELECT_TIMEOUT_SEC 1
+#define SELECT_TIMEOUT_MICROSEC 0
 
 #define OUTPUT_CHANNEL stdout
 #define INPUT_CHANNEL stdin
@@ -18,6 +20,7 @@ int setup_server_socket();
 
 void cleanup_server(int *client_fd, int num_connected, int socket_fd);
 
+int get_available_sockets(fd_set *sockets, int *monitored_descriptors, size_t num_monitored);
 
 
 
